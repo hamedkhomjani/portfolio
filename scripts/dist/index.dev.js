62379d3571b3beb5bf61c17ef2339dd124f1801e
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-11-06 23:58:19 +0100

    jump button and some script for nav btns

--- scripts/dist/index.dev.js
+++ scripts/dist/index.dev.js
@@ -1,7 +1,30 @@
 "use strict";
 
 var navigationElement = document.querySelector("#navigation");
-var worksElement = document.querySelector("#works");
+var worksElement = document.querySelector("#works"); //vvvvvvvvvvvvvvvvvvvv Jump button script vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv//
+//Get the button
+
+var mybutton = document.querySelector("#myBtn"); // When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+} // When the user clicks on the button, scroll to the top of the document
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+} //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^//
+
+
 fetch("/assets/data/data.json").then(function (response) {
   return response.json();
 }).then(function (data) {
